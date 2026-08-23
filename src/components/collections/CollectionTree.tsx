5964fbec1cdaf09bbcb64bd82dd34a2c53f2c476
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-23 14:52:11 +0000

    Aug 23, 2026, 8:22 PM

--- src/components/collections/CollectionTree.tsx
+++ src/components/collections/CollectionTree.tsx
@@ -743,7 +743,7 @@ export function CollectionTree() {
                       size="icon"
                       className="h-7 w-7"
                       title="New request"
-                      onClick={() => onNewRequest(folderMenuTarget.id)}
+                      onClick={() => HandleNewRequest(folderMenuTarget.id)}
 
                     >
                       <Plus className="h-3.5 w-3.5" />
