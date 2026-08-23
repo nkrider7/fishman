663f87cc496b9e4acd9caf5b0a9d8bbabda5cc12
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-23 14:25:09 +0000

    Aug 23, 2026, 7:55 PM

--- src/components/request/RequestBuilder.tsx
+++ src/components/request/RequestBuilder.tsx
@@ -244,6 +244,7 @@ export function RequestBuilder({ tabId }: RequestBuilderProps) {
                   title={def?.tooltip}
                 >
                   {m}
+                  
                 </SelectItem>
               );
             })}
