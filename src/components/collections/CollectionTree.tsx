c8652161208ef62f0d3e2b1cefb8712c69b78a47
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-23 14:47:22 +0000

    Aug 23, 2026, 8:17 PM

--- src/components/collections/CollectionTree.tsx
+++ src/components/collections/CollectionTree.tsx
@@ -106,10 +106,7 @@ const treeCollisionDetection: CollisionDetection = (args) => {
 
   return collisions;
 };
-const dispatch = useAppDispatch();
-const handleNewTab = () => {
-    dispatch(openRequestTab({ request: createEmptyRequest(), forceNew: true }));
-};
+
 const TREE_INDENT = 16;
 
 /** Vertical guide lines showing tree depth (Postman-style) */
