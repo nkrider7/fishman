a736b394b548cf891df223ef0b19dacd9e0ebb46
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-23 14:31:02 +0000

    Aug 23, 2026, 8:01 PM

--- src/components/collections/CollectionTree.tsx
+++ src/components/collections/CollectionTree.tsx
@@ -729,6 +729,7 @@ export function CollectionTree() {
                       startRename("folder", folder.id, folder.name);
                     }}
                   >
+                    <div>hey</div>
                     {isExpanded ? (
                       <FolderOpen className="h-4 w-4 shrink-0 text-amber-500" />
                     ) : (
