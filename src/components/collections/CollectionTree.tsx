d49b56bc4c12d6416421aa9819d1a94308f2203e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-23 14:44:57 +0000

    Aug 23, 2026, 8:14 PM

--- src/components/collections/CollectionTree.tsx
+++ src/components/collections/CollectionTree.tsx
@@ -743,7 +743,7 @@ export function CollectionTree() {
                       size="icon"
                       className="h-7 w-7"
                       title="New request"
-                      onClick={handleNewTab}
+                      onClick={handleNew}
 
                     >
                       <Plus className="h-3.5 w-3.5" />
